3ea5c9abadc7c7fd34fbb8cc2d4ac44317ea4bc9
David Temple <dstemple7@gmail.com>
2020-09-11 10:57:30 -0500

    names works

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from bst import BSTNode
 
 start_time = time.time()
 
@@ -12,11 +13,15 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+wicked_fast_names = BSTNode("yeet")
+
+for i in names_1:
+    wicked_fast_names.insert(i)
+for i in names_2:
+    if wicked_fast_names.contains(i):
+        duplicates.append(i)
+    else:
+        wicked_fast_names.insert(i)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
